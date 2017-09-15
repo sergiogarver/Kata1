b704b0461c5fa70903489c9804d06faf0f53fdd3
usuario <usuario@10.22.148.60>
2017-09-15 11:21:06 +0100

    Kata1 versión 2 : se usa Calendar en lugar de Date.

--- src/kata1/Kata1.java
+++ src/kata1/Kata1.java
@@ -1,12 +1,15 @@
 
 package kata1;
 
-import java.util.Date;
+import java.util.Calendar;
+import java.util.GregorianCalendar;
 
 public class Kata1 {
 
     public static void main(String[] args) {
-        Person person = new Person("Pepito", new Date(01,9,21));
-        System.out.println(person.getName()+ " tiene " + person.getAge()+ " años ");
+        Calendar date = GregorianCalendar.getInstance();
+        date.set(2001, 9, 24);
+        Person person = new Person("Pepito",date);
+        System.out.println(person.getName() + " tiene " + person.getAge() + " años.");
     }
 }
